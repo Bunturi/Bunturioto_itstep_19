ca5c680952faba05821d1da72f6a9b0a9d2f9fc9
bunturioto <Bunturioto@gmail.com>
2024-02-21 18:36:31 +0400

    add method witch checks if stack is empty

--- stack.py
+++ stack.py
@@ -10,3 +10,7 @@ class Stack:
         # Initialize an empty stack with top_node pointing to None and length set to 0
         self.top_node = None
         self.length = 0
+
+    def empty(self):
+        # Check if the stack is empty
+        return self.length == 0
