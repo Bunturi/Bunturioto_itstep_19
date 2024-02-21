0f05e9fd7ef29479f0220885479921b67660b054
bunturioto <Bunturioto@gmail.com>
2024-02-21 18:35:29 +0400

    create class stack and Initialize an empty stack

--- stack.py
+++ stack.py
@@ -3,3 +3,10 @@ class Node:
         # Initialize a node with data and set next pointer to None
         self.data = data
         self.next = None
+
+
+class Stack:
+    def __init__(self):
+        # Initialize an empty stack with top_node pointing to None and length set to 0
+        self.top_node = None
+        self.length = 0
