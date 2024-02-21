caf34b4dd517e9b7f13d202b59b2862e938a7a8d
bunturioto <Bunturioto@gmail.com>
2024-02-21 17:04:16 +0400

    adding append method to add new node end of list or if list empty make ne node the head

--- linklist.py
+++ linklist.py
@@ -9,3 +9,12 @@ class Linklist:
     def __init__(self):
         # Initializing an empty linked list with head pointing to None
         self.head = None
+
+    def append(self, data):
+        # Adding a new node to the end of the linked list
+        new_node = Node(data)
+
+        if self.head is None:
+            # If the list is empty, make the new node the head
+            self.head = new_node
+            return
