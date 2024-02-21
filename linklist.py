6669e291ecc72c4ca37568f7003d3262500a0356
bunturioto <Bunturioto@gmail.com>
2024-02-21 16:59:53 +0400

    creating class Linklist and Initializing an empty linked list with head pointing to None

--- linklist.py
+++ linklist.py
@@ -3,3 +3,9 @@ class Node:
         # Initializing a Node with data and setting next pointer to None
         self.data = data
         self.next = None
+
+
+class Linklist:
+    def __init__(self):
+        # Initializing an empty linked list with head pointing to None
+        self.head = None
